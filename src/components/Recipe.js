38e25eef890ed7c143f5a06f292175906a26c592
Milene <badouxmilene@gmail.com>
2017-10-19 12:44:09 -0700

    refactor: remove an unusued state

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,22 +4,6 @@ import DeleteButton from './DeleteButton'
 import EditButton from './EditButton'
 
 class Recipe extends Component {
-  state = {
-    currentRecipe:'',
-  }
-
-  handleDeleteEvent = (title) => {
-    this.props.onRemoveBtnClick(title)
-  }
-
-  handleClick = (e) => {
-    let currentRecipe = ''
-    e.preventDefault()
-    this.state.currentRecipe === e.target.innerText
-    ? currentRecipe = ''
-    : currentRecipe = e.target.innerText
-    this.setState(() => ({currentRecipe}))
-  }
 
   render() {
     const {onRemoveBtnClick, recipe, onEditBtnClick} = this.props
